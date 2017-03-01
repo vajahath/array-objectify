d42bb8f313f1889193c125b0bcecebb08278912f
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-03-01 18:03:03 +0530

    adds tests

---
+++ index.spec.js
@@ -0,0 +1,217 @@
+/*global describe:true it:true*/
+
+var expect = require('chai').expect;
+var isEqual = require('lodash').isEqual;
+var arrayObjectify = require('./index');
+
+describe('Array to object converter', function() {
+	it('successfully converts array to object with keepDuplications=true', function(done) {
+		var data = [{
+			fruit: 'apple',
+			color: 'yellow',
+			content: 'Vitamin Y',
+			nickName: 'Y-diple',
+		}, {
+			fruit: 'apple',
+			color: 'yellow',
+			content: 'Vitamin X',
+			nickName: 'X-diple',
+		}, {
+			fruit: 'mango',
+			color: 'red',
+			content: 'Vitamin yellow',
+			nickName: 'yellow-mango',
+		}, {
+			fruit: 'mango',
+			color: 'red',
+			content: 'Vitamin yellow',
+			nickName: 'yellow-mango-dupe',
+		}, {
+			fruit: 'apple',
+			color: 'green',
+			content: 'Vitamin green',
+			nickName: 'green-mango',
+		}];
+
+		var hierarchy = ['fruit', 'color'];
+
+		var expectedOutput = {
+			'apple': {
+				'yellow': {
+					'content': [
+						'Vitamin Y',
+						'Vitamin X'
+					],
+					'nickName': [
+						'Y-diple',
+						'X-diple'
+					]
+				},
+				'green': {
+					'content': [
+						'Vitamin green'
+					],
+					'nickName': [
+						'green-mango'
+					]
+				}
+			},
+			'mango': {
+				'red': {
+					'content': [
+						'Vitamin yellow',
+						'Vitamin yellow'
+					],
+					'nickName': [
+						'yellow-mango',
+						'yellow-mango-dupe'
+					]
+				}
+			}
+		};
+
+		var objectified = arrayObjectify(hierarchy, data, true);
+
+		expect(isEqual(expectedOutput, objectified)).to.be.true;
+		done();
+	});
+	it('successfully converts array to object with keepDuplications=false', function(done) {
+		var data = [{
+			fruit: 'apple',
+			color: 'yellow',
+			content: 'Vitamin Y',
+			nickName: 'Y-diple',
+		}, {
+			fruit: 'apple',
+			color: 'yellow',
+			content: 'Vitamin X',
+			nickName: 'X-diple',
+		}, {
+			fruit: 'mango',
+			color: 'red',
+			content: 'Vitamin yellow',
+			nickName: 'yellow-mango',
+		}, {
+			fruit: 'mango',
+			color: 'red',
+			content: 'Vitamin yellow',
+			nickName: 'yellow-mango-dupe',
+		}, {
+			fruit: 'apple',
+			color: 'green',
+			content: 'Vitamin green',
+			nickName: 'green-mango',
+		}];
+
+		var hierarchy = ['fruit', 'color'];
+
+		var expectedOutput = {
+			'apple': {
+				'yellow': {
+					'content': [
+						'Vitamin Y',
+						'Vitamin X'
+					],
+					'nickName': [
+						'Y-diple',
+						'X-diple'
+					]
+				},
+				'green': {
+					'content': [
+						'Vitamin green'
+					],
+					'nickName': [
+						'green-mango'
+					]
+				}
+			},
+			'mango': {
+				'red': {
+					'content': [
+						'Vitamin yellow'
+					],
+					'nickName': [
+						'yellow-mango',
+						'yellow-mango-dupe'
+					]
+				}
+			}
+		};
+
+		var objectified = arrayObjectify(hierarchy, data, false);
+
+		expect(isEqual(expectedOutput, objectified)).to.be.true;
+		done();
+	});
+
+	it('successfully converts array to object without keepDuplications flag (equivallent to false)', function(done) {
+		var data = [{
+			fruit: 'apple',
+			color: 'yellow',
+			content: 'Vitamin Y',
+			nickName: 'Y-diple',
+		}, {
+			fruit: 'apple',
+			color: 'yellow',
+			content: 'Vitamin X',
+			nickName: 'X-diple',
+		}, {
+			fruit: 'mango',
+			color: 'red',
+			content: 'Vitamin yellow',
+			nickName: 'yellow-mango',
+		}, {
+			fruit: 'mango',
+			color: 'red',
+			content: 'Vitamin yellow',
+			nickName: 'yellow-mango-dupe',
+		}, {
+			fruit: 'apple',
+			color: 'green',
+			content: 'Vitamin green',
+			nickName: 'green-mango',
+		}];
+
+		var hierarchy = ['fruit', 'color'];
+
+		var expectedOutput = {
+			'apple': {
+				'yellow': {
+					'content': [
+						'Vitamin Y',
+						'Vitamin X'
+					],
+					'nickName': [
+						'Y-diple',
+						'X-diple'
+					]
+				},
+				'green': {
+					'content': [
+						'Vitamin green'
+					],
+					'nickName': [
+						'green-mango'
+					]
+				}
+			},
+			'mango': {
+				'red': {
+					'content': [
+						'Vitamin yellow'
+					],
+					'nickName': [
+						'yellow-mango',
+						'yellow-mango-dupe'
+					]
+				}
+			}
+		};
+
+		var objectified = arrayObjectify(hierarchy, data);
+
+		expect(isEqual(expectedOutput, objectified)).to.be.true;
+		done();
+	});
+});
